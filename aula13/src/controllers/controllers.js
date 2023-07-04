4f0fa0e81d90340e6ad5f1c4a550de6b709c6247
Leonardo da Costa da Silva <leonardo.costa.silva@g.globo>
2023-07-04 20:25:06 -0300

    Aula13: introdução ao MVC

---
+++ aula13/src/controllers/controllers.js
@@ -0,0 +1,6 @@
+// chamar a model (instanciar)
+// chamar as minhas views
+
+// Criar uma lógica que utiliza as duas pastas e envia informação para o HTML
+
+new Cliente("","","")
---
+++ aula13/src/models/models.js
@@ -0,0 +1,7 @@
+class Cliente {
+    constructor(nome, cpf, telefone){
+        this.nome = nome,
+        this.cpf = cpf,
+        this.telefone = telefone
+    }
+}
